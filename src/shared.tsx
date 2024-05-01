c0040e7a456ecd0ec58fe241cdcc34cb6ccf2cce
Ondřej Musil <mail@ondrejmusil.cz>
2024-05-01 07:33:32 +0200

    use next_public and vercel branch url env vars to demonstrate bad DX

--- src/shared.tsx
+++ src/shared.tsx
@@ -1,10 +1,13 @@
 export function Page() {
   const env = process.env.MY_ENV_VAR;
+  const publicEnv = process.env.NEXT_PUBLIC_MY_ENV_VAR;
 
   return (
     <pre>
       <code>
         ENV: {JSON.stringify(env, null, 2)}
+        <br />
+        PUBLIC ENV: {JSON.stringify(publicEnv, null, 2)}
       </code>
     </pre>
   );
